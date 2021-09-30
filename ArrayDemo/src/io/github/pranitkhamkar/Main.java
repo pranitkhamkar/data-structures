9591e769b39823f6122885d6536234f83c51bd89
pranitkhamkar <88722447+pranitkhamkar@users.noreply.github.com>
2021-09-30 14:39:55 +0530

    fix: polish

--- ArrayDemo/src/io/github/pranitkhamkar/Main.java
+++ ArrayDemo/src/io/github/pranitkhamkar/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
      System.out.println("asdasd");
-        System.out.println("asdasd");
-        System.out.println("asdasd");
-        System.out.println("asdasd");
-        System.out.println("asdasd");
+     System.out.println("asdasd");
+     System.out.println("asdasd");
+     System.out.println("asdasd");
+     System.out.println("asdasd");
 
     }
 }
