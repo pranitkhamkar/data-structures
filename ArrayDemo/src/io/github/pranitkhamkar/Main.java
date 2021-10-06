997687c0958fb8c5a1a351607bcc731d994c5c6d
Pranit Khamkar <pranitbkhamkar@gmail.com>
2021-10-06 21:18:46 +0530

    docs: class example

--- ArrayDemo/src/io/github/pranitkhamkar/Main.java
+++ ArrayDemo/src/io/github/pranitkhamkar/Main.java
@@ -2,13 +2,12 @@ package io.github.pranitkhamkar;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-     System.out.println("asdasd");
-     System.out.println("asdasd");
-     System.out.println("asdasd");
-     System.out.println("asdasd");
-     System.out.println("asdasd");
-
+    public static void main(String[] args)
+    {
+        int[] arrays = {1,2,3,4,5};
+        int[] arrays2 = new int[10];
+        for(int i=0; i<array.length; i++)
+        {
+        System.out.println(array[i]);
     }
 }
